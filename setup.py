a2795f22103fa734a50649b564d6e838566b5d4a
J-KEI <kei@j-kei.net>
2026-07-01 23:55:01 +0900

    Add MANIFEST.in and bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='octodns-xserver',
-    version='0.0.1',
+    version='0.0.2',
     description='XServer DNS provider for octoDNS',
     long_description=long_description,
     long_description_content_type='text/markdown',
